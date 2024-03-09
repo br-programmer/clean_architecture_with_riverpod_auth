ff9f301d1b521fdea65d2cc3c363bc44e5a604f6
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-09 18:23:48 -0500

    📝 (splash_screen.dart): organize imports and fix import statements to improve code readability and maintainability

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,8 @@
-import 'package:clean_architecture_with_riverpod/extensions/extensions.dart';
-import 'package:clean_architecture_with_riverpod/screens/screens.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../extensions/extensions.dart';
+import 'screens.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
