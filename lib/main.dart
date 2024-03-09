d746cebcc5464c85ede87fbf96391645560ece44
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-09 12:36:50 -0500

    🔧 (main.dart): import 'firebase_options.dart' to use DefaultFirebaseOptions.currentPlatform ✨ (main.dart): initialize Firebase using Firebase.initializeApp() with DefaultFirebaseOptions.currentPlatform

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:clean_architecture_with_riverpod/firebase_options.dart';
 import 'package:clean_architecture_with_riverpod/screens/screens.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
