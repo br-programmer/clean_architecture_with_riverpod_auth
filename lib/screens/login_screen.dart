e2ce3ab2898fd0b7e92c46fb8565e587dd488e73
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-09 18:24:08 -0500

    📝 (login_screen.dart): update imports to use relative paths for better maintainability ♻️ (login_screen.dart): refactor code to use context extensions for accessing colorScheme and separator height

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:developer';
 
-import 'package:clean_architecture_with_riverpod/extensions/extensions.dart';
-import 'package:clean_architecture_with_riverpod/screens/screens.dart';
-import 'package:clean_architecture_with_riverpod/widgets/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../extensions/extensions.dart';
+import '../extensions/num_x.dart';
+import '../widgets/widgets.dart';
+import 'screens.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -38,8 +40,8 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    const separator = SizedBox(height: 28);
+    final colorScheme = context.colorScheme;
+    final separator = 28.h;
     return Scaffold(
       body: SafeArea(
         child: Padding(
