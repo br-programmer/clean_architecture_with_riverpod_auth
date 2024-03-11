5af460bafa62bdc0275dcb3e1bcf1403531f3124
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-11 09:01:50 -0500

    ✨ (home_screen.dart): import necessary extensions, services, and widgets to improve code organization and maintainability ♻️ (home_screen.dart): refactor the body of the Scaffold to use a Column with a CustomButton for better UI layout and functionality ✅ (home_screen.dart): add logout functionality to the CustomButton onPressed event to allow users to logout from the app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../extensions/extensions.dart';
+import '../services/firebase_auth_service.dart';
+import '../widgets/widgets.dart';
+import 'screens.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -7,9 +12,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButton(
+              onPressed: () => FirebaseService.instance.logout().whenComplete(
+                    () => context.pushAndRemoveUntil(AuthScreen.route),
+                  ),
+              text: 'Logout',
+            ),
+          ],
+        ),
       ),
     );
   }
