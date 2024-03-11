cf3623a0be7de560d7c0cfc94363fef30ce03433
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-11 09:00:57 -0500

    ♻️ (num_x.dart): refactor NumX extension to use toDouble() method instead of casting to double 💡 (num_x.dart): improve type safety by using toDouble() method instead of casting to double

--- lib/extensions/num_x.dart
+++ lib/extensions/num_x.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 extension NumX on num {
-  SizedBox get w => SizedBox(width: this as double);
-  SizedBox get h => SizedBox(height: this as double);
+  SizedBox get w => SizedBox(width: toDouble());
+  SizedBox get h => SizedBox(height: toDouble());
 }
