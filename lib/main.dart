0ac0df42956944bbb6c2626f6bfa1dd979f4b9e1
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-13 09:52:09 -0500

    📝 (main.dart): update theme configuration to use a custom theme defined in AppTheme class ♻️ (main.dart): remove unnecessary Firebase initialization code as it is not being used in the app ✨ (main.dart): update route names to match the renamed screen classes for better code organization and readability

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
 import 'screens/screens.dart';
+import 'theme/theme.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
@@ -17,18 +15,12 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF262626),
-        colorScheme: const ColorScheme.dark().copyWith(
-          primary: const Color(0xFF58AE7A),
-          secondary: const Color(0xFF454545),
-        ),
-      ),
+      theme: AppTheme.theme,
       routes: <String, WidgetBuilder>{
         SplashScreen.route: (_) => const SplashScreen(),
         AuthScreen.route: (_) => const AuthScreen(),
-        LoginScreen.route: (_) => const LoginScreen(),
-        RegisterScreen.route: (_) => const RegisterScreen(),
+        SignInScreen.route: (_) => const SignInScreen(),
+        SignUpScreen.route: (_) => const SignUpScreen(),
         HomeScreen.route: (_) => const HomeScreen(),
       },
     );
